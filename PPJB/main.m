089b0009d77e208cad3c553cfd270a669676c3f9
xungeng <xungeng@xungengdeMacBook-Pro.local>
2016-12-16 00:12:25 +0800

    jb

--- PPJB/main.m
+++ PPJB/main.m
@@ -8,8 +8,14 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
+#include <unistd.h>
+
+char *
+getcwd(char *buf, size_t size);
 
 int main(int argc, char * argv[]) {
+    char buf[1024];
+    printf("%s\n", getcwd(buf, sizeof(buf)));
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
